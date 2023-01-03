cd79eeaf81ddaa2c36c4a80ffc450083ae5df752
junhyung8795 <junhyung8795@naver.com>
2023-01-03 18:21:33 +0900

    Making Simple Login using localStorage. I'm not sure how to use GMKEY(accessCode that I'm gonna use). I think I make new model(accessCode) and everytime we login, I should make browser find the accessCode in the mongoDB and compare with the accessCode that user writes.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App({ Component, pageProps }) {
     return (
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
 import Seo from "../components/Seo";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Home() {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+    const router = useRouter();
+
+    const [accessCode, setAccessCode] = useState("");
+
+    const handleSubmit = (e) => {
+        if (accessCode === "GM29") {
+            e.preventDefault();
+            localStorage.setItem("accessCode", accessCode);
+            console.log(typeof accessCode);
+            router.push("/about");
+        }
+    };
     return (
-        <div>
+        <div className="p-3 mb-2 bg-black text-white">
             <Seo title="Home" />
             <div className="title">
                 <h1>Graymood Timetable</h1>
             </div>
-            <form>
-                <button></button>
+            <form className="input-group mb-3" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="write the code"
+                    aria-label="Username"
+                    aria-describedby="basic-addon1"
+                    name="accessCode"
+                    onChange={({ target }) => setAccessCode(target.value)}
+                />
+                <div className="col-12">
+                    <button className="btn btn-primary" type="submit">
+                        Login
+                    </button>
+                </div>
             </form>
-            <style jsx>{`
-                .title {
-                    display: flex;
-                    justify-content: center;
-                }
-            `}</style>
         </div>
     );
 }
