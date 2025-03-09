07e21a2ee7566a45aae9f1f38df3a3db4bb1e295
junhyung8795 <junhyung8795@naver.com>
2025-03-09 12:31:06 +0900

    Delete Database connection console.log()

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,14 +1,19 @@
 import mongoose from "mongoose";
 
 const connection = {};
+const MONGODB_URL = process.env.NEXT_PUBLIC_MONGODB_URL;
+
+if (!MONGODB_URL) {
+    throw new Error(
+        "Please define the MONGODB_URI environment variable inside .env.local"
+    );
+}
 
 export default async function dbConnect() {
     if (connection.isConnected) {
-        console.log("Using existing connection");
         return;
     }
     mongoose.set("strictQuery", false);
     const db = await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URL);
     connection.isConnected = db.connections[0].readyState;
-    console.log("Connected to MongoDB");
 }
